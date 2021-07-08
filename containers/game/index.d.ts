339072ca36af19a01379aab6c616b54d380fe998
VilmerBeep <hizuvia@gmail.com>
2021-07-08 10:24:44 +0200

    Experimenting with device rotation

--- containers/game/index.d.ts
+++ containers/game/index.d.ts
@@ -1,1 +1,4 @@
-export {};
+declare const debug_text: HTMLParagraphElement;
+declare const demo_button: HTMLButtonElement;
+declare let is_running: boolean;
+declare function handleOrientation(event: DeviceOrientationEvent): void;
