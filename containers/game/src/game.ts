7a9803da9d24222e07b8dea84d74763055aeafc1
VilmerBeep <hizuvia@gmail.com>
2021-07-16 13:01:37 +0200

    Changed setup and got dev improvements

--- containers/game/src/game.ts
+++ containers/game/src/game.ts
@@ -3,26 +3,54 @@ import { Scene } from "@babylonjs/core/scene";
 import { ArcRotateCamera } from "@babylonjs/core/Cameras";
 import { Vector3 } from "@babylonjs/core/Maths/math";
 import { HemisphericLight } from "@babylonjs/core/Lights"
-import { Mesh, MeshBuilder } from "@babylonjs/core/Meshes";
+import { MeshBuilder } from "@babylonjs/core/Meshes";
+import { Tools } from "@babylonjs/core";
 
-var canvas: any = document.getElementById("renderCanvas");
-var engine: Engine = new Engine(canvas, true);
+export const Game = () => {
+    var canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+    var engine = new Engine(canvas, true);
 
-function createScene(): Scene {
-    var scene: Scene = new Scene(engine);
+    var testScene: Scene = createTestScene(engine, canvas);
 
-    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+    //Run render loop
+    engine.runRenderLoop(() => {
+
+        //Render scene
+        testScene.render();
+    });
+
+    // Watch for browser/canvas resize events
+    window.addEventListener("resize", function () {
+        engine.resize();
+    });
+}
+
+
+const createTestScene = (engine: Engine, canvas: HTMLCanvasElement): Scene => {
+    //Initialize
+    const scene: Scene = new Scene(engine);
+
+    //Camera
+    const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 10, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
-    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    //Add scene elements
+    const light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
-    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+    const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+
+    //House
+    const roof = MeshBuilder.CreateCylinder("roof", { diameter: 1.8, height: 3.4, tessellation: 3 }, scene);
+    roof.position.y = 1 + 1.8 / 2;
+    roof.rotation.x = Tools.ToRadians(90);
+
+    const box = MeshBuilder.CreateBox("box", { width: 3, height: 1, depth: 1.5 }, scene);
+    box.position.y = .5;
+
+    //Run before rendering the scene
+    scene.onBeforeRenderObservable.add(() => {
+
+    });
 
     return scene;
 }
-
-var scene: Scene = createScene();
-
-engine.runRenderLoop(() => {
-    scene.render();
-});
--- containers/game/src/index.ts
+++ containers/game/src/index.ts
@@ -1,31 +1,3 @@
-console.log("Ver: 6");
+import { Game } from "./game";
 
-import { MotionManager } from "./modules/input";
-
-
-const demo_button = document.getElementById("startButton");
-const textRot = document.getElementById("rot");
-const textMov = document.getElementById("mov");
-const textMovRaw = document.getElementById("raw");
-demo_button.addEventListener("click", (e: Event) => {
-    const motionManager = new MotionManager();
-
-    update(motionManager);
-});
-
-let smoothed = 0;
-
-function update(motionManager: MotionManager) {
-    textRot.innerText = Math.round(motionManager.rotation).toString();
-    textMovRaw.innerText = Math.round(motionManager.movement).toString();
-
-    smoothed = lerp(smoothed, motionManager.movement, 0.2);
-
-    textMov.innerText = Math.round(smoothed).toString();
-
-    setTimeout(() => { update(motionManager); }, 16);
-}
-
-function lerp(start: number, end: number, amt: number) {
-    return (1 - amt) * start + amt * end
-}
+Game();
